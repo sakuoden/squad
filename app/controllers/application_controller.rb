124cbc4acd55ae563d11128f9088214880d29c4b
Daisuke SAkusabe <4oden18@gmail.com>
2018-12-07 03:38:49 +0000

    fix destination after log_out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+ 	protected
+
+ 	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+		devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+	end
+
+
+	def after_sign_out_path_for(resource_or_scope)
+		user_session_path
+	end
+
 end

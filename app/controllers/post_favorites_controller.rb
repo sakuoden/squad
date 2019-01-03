d5e319c029234368733e70672457d3a83b2977a7
Daisuke SAkusabe <4oden18@gmail.com>
2019-01-03 13:51:58 +0000

    fix_last

--- app/controllers/post_favorites_controller.rb
+++ app/controllers/post_favorites_controller.rb
@@ -1,21 +1,13 @@
 class PostFavoritesController < ApplicationController
 	def create
 		@post = Post.find(params[:post_id])
-		if PostFavorite.where(post_favorite_user: current_user.id).exists?
-			@post_favorite = PostFavorite.find_by(post_favorite_user: current_user.id)
-			@post_favorite_middle = PostFavoriteMiddle.new(post_id: @post.id, post_favorite_id: @post_favorite.id)
-			@post_favorite_middle.save
-		else
-			@post.post_favorites.new(post_favorite_user: current_user.id)
-			@post.save
-		end
+		@post.post_favorite_middles.new(post_favorite_id: current_user.id)
+		@post.save
 	end
 
 	def destroy
 		@post = Post.find(params[:id])
-		@post_favorite = PostFavorite.find_by(post_favorite_user: current_user.id)
-		@post_favorite_middles = PostFavoriteMiddle.where(post_id: @post.id)
-		@post_favorite_middle = @post_favorite_middles.find_by(post_favorite_id: @post_favorite.id)
-		@post_favorite_middle.destroy
+		@post.post_favorite_middles.find_by(post_favorite_id: current_user.id)
+		@post.destroy
 	end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
 	has_many :post_favorite_middles, dependent: :destroy
-	has_many :post_favorites, through: :post_favorite_middles
 
 	belongs_to :user
 	belongs_to :team
